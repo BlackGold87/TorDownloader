2b62011b362dda536a17a7f4b97bc3e15eec32ba
Edoardo <edoardo.emili@gmail.com>
2016-10-26 11:56:32 +0200

    Added Converter

---
+++ NewTor/Converter/FileSizeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NewTor.Converter
+{
+    public class FileSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var byteCount = (long)value;
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+
+            if (byteCount == 0)
+                return "0" + suf[0];
+
+            var bytes = Math.Abs(byteCount);
+            var place = System.Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+            return (Math.Sign(byteCount) * num) + suf[place];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
